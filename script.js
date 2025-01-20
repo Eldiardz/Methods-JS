e57bf09c5ca005df090090d5e1c1c8df7709a11b
Eldiardz <eldiardzumabekov@gmail.com>
2025-01-20 14:59:17 +0600

    new methods js

--- script.js
+++ script.js
@@ -58,3 +58,42 @@ console.log(Object.values(gol1)); // [1, 2]
 
 let gol2 = {a: 1, b: 2}
 console.log(Object.entries(gol2))
+
+console.log(Math.abs(-123)) // 123
+
+console.log(Math.ceil(1.4)); // 2
+
+console.log(Math.floor(1.8)); // 1
+
+console.log(Math.max(1, 2, 3, 4)); // 4
+
+console.log(Math.random()); // Случайное число между 0 и 1 
+
+let date = new Date()
+console.log(date.getDate()); // Нынешнее число месяца (1-31)
+
+let date1 = new Date()
+console.log(date1.getFullYear()); // Нынешний год (2025)
+
+let date2 = new Date()
+console.log(date2.getTime()); // Значение миллисекунд которые прошли с 1 января 1970 года
+
+let date3 = new Date()
+date3.setDate(14);
+console.log(date3); // Дата смещается до 14-ого числа этого месяца
+
+let jsonString = '{"Имя": "Джон", "возраст": 30}'
+let obj = JSON.parse(jsonString)
+console.log(obj); // {Имя: 'Джон', возраст: 30}
+
+let ob1 = '{ имя: "Джон" "возраст": 30}';
+let jsonString1 = JSON.stringify(obj);
+console.log(jsonString); // {"Имя": "Джон", "возраст": 30}
+
+let p1 = Promise.resolve(1)
+let p2 = Promise.resolve(2)
+Promise.all([p1, p2]).then(result => console.log(result)); // [1, 2]
+
+let p3 = new Promise((resolve) => setTimeout(resolve, 500, "Один"));
+let p4 = new Promise((resolve) => setTimeout(resolve, 100, "Два"));
+Promise.race([p3, p4]).then(result => console.log(result));  // Два
